2c5dc6e0abe3a90ea5a6eb42c4ffdd8e626f1569
Daniel <Daniel@Daniels-PC.gateway>
2017-03-09 19:39:57 +1100

    printLogin commit

--- HotelSystem/src/HotelSystem.java
+++ HotelSystem/src/HotelSystem.java
@@ -3,7 +3,19 @@ public class HotelSystem {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hotel Management System. Testing..");
+		printLogin();
+	}
+	
+	public static void printLogin() {
+		System.out.println("=====================================");
+		System.out.println("   John Electrician Booking System");
+		System.out.println("=====================================");
+		System.out.println("\t\tLogin");
+		System.out.println("=====================================");
+		System.out.println("Please enter username: username00125");
+		System.out.println("Please enter password: tgj*usLf");
+		System.out.println("=====================================");
+		
 	}
 
 }
