910dcfed05c80f5498ee1ca5f7494a69eeb73b66
Daniel <Daniel@Daniels-PC>
2017-03-14 19:59:35 +1100

    menu complete commit

--- BookingSystem/src/BookingSystem.java
+++ BookingSystem/src/BookingSystem.java
@@ -1,9 +1,46 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
 
 public class BookingSystem {
 
+	final static Scanner scan = new Scanner(System.in);
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		printLogin();
+		int val = 0;
+		boolean done = false;
+		while (!done) {
+			printLogin();
+				try {
+					System.out.println("Choose an option: ");
+					val = scan.nextInt();
+				} catch(InputMismatchException exception) {
+					System.out.println("Input is not an integer");
+					scan.nextLine();
+					val = -1;
+				}
+
+			switch ( val ) {
+				case -1:
+					done = false;
+					break;
+				case 1:
+					System.out.println("You entered Login");
+					done = false;
+					break;
+				case 2:
+					System.out.println("You entered Register");
+					done = false;
+					break;
+				case 3:
+					System.out.println("You entered Quit");
+					done = true;
+					break;
+				default:
+					System.out.println("Invalid Input");
+			}
+		}
+		System.out.println("\nProgram has been terminated.");
 	}
 	
 	public static void printLogin() {
@@ -12,22 +49,8 @@ public class BookingSystem {
 		System.out.println("=====================================");
 		System.out.println("1. Login");
 		System.out.println("2. Register");
+		System.out.println("3. Quit");
 		System.out.println("=====================================");
-		System.out.println("\tLogin");
-		System.out.println("=====================================");
-		System.out.println("Please enter username: username00125");
-		System.out.println("Please enter password: tgj*usLf");
-		System.out.println("=====================================");
-		System.out.println("\t       Register");
-		System.out.println("=====================================");
-		System.out.println("First Name: ");
-		System.out.println("Last Name: ");
-		System.out.println("Address: ");
-		System.out.println("Mobile: ");
-		System.out.println("Email: ");
-		System.out.println("Password: ");
-		System.out.println("=====================================");
-		
 	}
 
 }
