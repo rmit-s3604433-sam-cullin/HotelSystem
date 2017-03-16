acd7819ef60ff925027a984da60060bcd8082b35
rmit-s3604433-sam-cullin <s3604433@student.rmit.edu.au>
2017-03-16 13:06:23 +1100

    update to person object removed some variables for ease

--- BookingSystem/src/Person.java
+++ BookingSystem/src/Person.java
@@ -3,34 +3,32 @@ import java.util.Date;
 
 public class Person{
 	private String name;
-	private String email;
-	private String number;
 	private String ID;
-	private Date bDay;
+	private String number;
+
 	
 	public Person(){
 		
 	}
-	public Person(String name,String email,String number,String ID,Date bDay){
+	public Person(String name,String number,String ID){
 		this.name = name;
-		this.email = email;
 		this.number = number;
 		this.ID = ID;
-		this.bDay = bDay;
+
 	}
-	
+	public String getContact(){
+		return number;
+	}
+	public void setContract(String number){
+		this.number = number;
+	}
 	public String getName() {
 		return name;
 	}
 	public void setName(String name) {
 		this.name = name;
 	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
+	
 	public String getNumber() {
 		return number;
 	}
@@ -43,12 +41,7 @@ public class Person{
 	public void setID(String iD) {
 		ID = iD;
 	}
-	public Date getbDay() {
-		return bDay;
-	}
-	public void setbDay(Date bDay) {
-		this.bDay = bDay;
-	}
+
 	public void print(){
 		
 	}
