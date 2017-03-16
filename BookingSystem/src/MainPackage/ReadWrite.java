b49b98be66950b7648d03503b4978eb4ad1cf817
Nicholas <nicholasooi.user@gmail.com>
2017-03-16 16:11:07 +1100

    Read/Write update code.

--- BookingSystem/src/MainPackage/ReadWrite.java
+++ BookingSystem/src/MainPackage/ReadWrite.java
@@ -1,48 +1,20 @@
-package MainPackage;
 import java.io.BufferedWriter;
 import java.io.FileWriter;
 import java.io.IOException;
 
 public class ReadWrite {
 
-	public Employee AddEmployee(Employee employee) {
+	public void Write(String data, String location) {
 		BufferedWriter writer = null;
 		try {
-			writer = new BufferedWriter(new FileWriter("C:/Users/Nic/git/HotelSystem/src/employee.txt", true));
+			writer = new BufferedWriter(new FileWriter(location, true));
 
-			writer.write(employee.getID() + ",");
-			writer.write(employee.getName() + ",");
-			writer.write(employee.getContact() + ",");
-			writer.write(employee.getPosition() + ",");
+			writer.write(data);
 			writer.newLine();
 			writer.flush();
 		} 
 		catch (IOException e) {
 				e.printStackTrace();
-		} 
-		finally {
-			if(writer != null) try {
-				writer.close();
-			} 
-			catch (IOException e2) {
-				e2.printStackTrace();
-			}
-		}
-	}
-	public Customer WriteCustomer(Customer customer) {
-		BufferedWriter writer = null;
-		try {
-			writer = new BufferedWriter(new FileWriter("C:/Users/Nic/git/HotelSystem5/BookingSystem/src/customerinfo.txt", true));
-			writer.write(name + ",");
-			writer.write(username + ",");
-			writer.write(password + ",");
-			writer.write(address + ",");
-			writer.write(contact);
-			writer.newLine();
-			writer.flush();
-		} 
-		catch (IOException e) {
-			e.printStackTrace();
 		} 
 		finally {
 			if(writer != null) try {
