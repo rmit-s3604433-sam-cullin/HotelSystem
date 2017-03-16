c269052f02a1cd557c51686f5ce8cc3c20340a52
rmit-s3604433-sam-cullin <s3604433@student.rmit.edu.au>
2017-03-16 13:53:19 +1100

    filled in booking.java

--- BookingSystem/src/Booking.java
+++ BookingSystem/src/Booking.java
@@ -1,4 +1,34 @@
 
-public class Booking {
+import java.util.Date;
+import java.util.LinkedList;
+
+
+
+public class Booking{
+
+	public void cancel() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void complete() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public LinkedList<Customer> getGoing() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public Date getDate() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean enterNextBooking() {
+		// TODO Auto-generated method stub
+		return false;
+	}
 
 }
