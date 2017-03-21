08b9c2e9d77a08f6d03ad6558be3294046a40c55
Daniel <Daniel@Daniels-PC.gateway>
2017-03-22 02:47:36 +1100

    small tweaks 03 commit

--- BookingSystem/src/junit/OwnerMenu.java
+++ BookingSystem/src/junit/OwnerMenu.java
@@ -1,12 +1,10 @@
 package junit;
 
-import static org.junit.Assert.*;
-
 import java.io.IOException;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.Assert.*;
 
 import views.Menu;
 
