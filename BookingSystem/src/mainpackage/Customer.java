6541aa1f87e60aa912f369115197138ccf45e4b4
rmit-s3599741-Khoa-Le <s3599741@student.rmit.edu.au>
2017-04-03 22:32:08 +1000

    Added Regex to customer

--- BookingSystem/src/mainpackage/Customer.java
+++ BookingSystem/src/mainpackage/Customer.java
@@ -12,26 +12,73 @@ public class Customer extends Person {
 	public void createBooking() {
 		// TODO Auto-generated method stub
 	}
+	
 	@Override
-	public void setID(String ID) {
-		this.ID = ID;
+	public void setID(String ID) { //can't have duplicates
+		while(true) {
+			if(ID.matches("[0-9]{3}")) {
+				ID.trim();
+				this.ID = ID;
+				break;
+			} else {
+				System.out.println("Invalid ID number");
+				break;
+			}
+		}		
 	}
+	
 	@Override
 	public void setName(String name) {
-		this.name = name;
+		while(true) {
+			if(name.matches("[a-zA-Z ]{1,30}")) {	
+				this.name = name;
+				break;
+			} else {
+				System.out.println("Invalid name");
+				break;
+			}
+		}
 	}
+	
+	@Override
+	public void setPassword(String password) {	
+		while(true) {	
+			if(password.matches("[a-zA-Z0-9,./';:?><{}|+=-_()*&^%$#@!`~]{8,20}")){
+				this.password = password;
+				break;
+			} else {
+				System.out.println("Invalid Password");
+				break;
+			}
+		}		
+	}
+	
 	@Override
 	public void setAddress(String address) {
-		this.address = address;
+		while(true) {	
+			if(address.matches("[a-zA-Z0-9 ,./':-|_`~&$@#+()]{2,40}")){
+				this.address = address;
+				break;
+			} else {
+				System.out.println("Invalid Address");
+				break;
+			}
+		}		
 	}
+	
 	@Override
 	public void setNumber(String number) {
-		this.number = number;
+		while(true) {
+			if(number.matches("[0-9]{10}")){
+				this.number = number;
+				break;
+			} else {
+				System.out.println("Invalid Number");
+				break;
+			}
+		}
 	}
-	@Override
-	public void setPassword(String password) {
-		this.password = password;
-	}
+
 	@Override
 	public String toString(){
 		return ID + "," + name + "," + password + "," + address + "," + number;
