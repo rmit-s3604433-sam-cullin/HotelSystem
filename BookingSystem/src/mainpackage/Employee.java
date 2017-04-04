affaadc4b45b811b75999b0f4521292f7791ff5e
Daniel <Daniel@Daniels-PC.gateway>
2017-04-04 17:03:53 +1000

    small fix commit

--- BookingSystem/src/mainpackage/Employee.java
+++ BookingSystem/src/mainpackage/Employee.java
@@ -7,31 +7,20 @@ public class Employee {
 	
 	private String ID, name, address, number;
 	
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-	public Employee(String ID, String name, String address, String number) {
-=======
->>>>>>> GUI
 	public Employee() {
 		
 	}
-	public Employee(String ID, String name, String address, String number, String age, String position) {
->>>>>>> Regex in setters and junit overhaul commit
+	public Employee(String ID, String name, String address, String number) {
 		this.ID = ID;
 		this.name = name;
 		this.address = address;
 		this.number = number;
-		/*this.age = age;
-		this.position = position;*/
 	}
 
 	public String getID() { return ID; }
 	public String getName() { return name; }
 	public String getAddress() { return address; }
 	public String getNumber() { return number; }
-	/*public String getAge() { return age; }
-	public String getPosition() { return position; }*/
 	
 	public void setID(String ID) {
 		this.ID = ID;
@@ -45,38 +34,9 @@ public class Employee {
 	public void setNumber(String number) {
 		this.number = number;
 	}
-	/*public void setAge(String age) {
-		this.age = age;
-	}
-	public void setPosition(String position) {
-		this.position = position;
-	}*/
 	
 	public String toString(){
 		return ID + "," + name +  "," + address + "," + number;
-	}
-	
-	
-	
-	
-	
-	
-	
-	public int multiply(int x, int y){
-		
-		return x*y;
-		
-	}
-	
-	
-	public void multiply(){
-		
-		Scanner scan = new Scanner(System.in);
-		
-		int x = scan.nextInt();
-		int y = scan.nextInt();
-		
-		int total = x*y;
 	}
 
 }
