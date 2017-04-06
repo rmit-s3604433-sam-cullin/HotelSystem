0a1f5ca0c1453862a82224a8c2f420f797852177
rmit-s3599741-Khoa-Le <s3599741@student.rmit.edu.au>
2017-04-06 15:28:34 +1000

    Moved login to another class

--- BookingSystem/src/view/Menu.java
+++ BookingSystem/src/view/Menu.java
@@ -1,13 +1,10 @@
-package view;
+package views;
 
-import java.awt.EventQueue;
 import java.io.*;
-import java.sql.*;
 import java.util.Scanner;
 
 import mainpackage.*;
 import register.AddCustomer;
-import register.AddCustomerV2;
 import register.AddEmployee;
 
 public class Menu {
@@ -15,19 +12,9 @@ public class Menu {
 	final static Scanner scan = new Scanner(System.in);
 	AddCustomer customer = new AddCustomer();
 	AddEmployee employee = new AddEmployee();
+	Login login = new Login();
 	
 	private String printMenu(){
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					Login window = new Login();
-					window.getFrame().setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-		
 		System.out.println("======================================");
 		System.out.println("   John's Electrician Booking System");
 		System.out.println("======================================");
@@ -47,7 +34,7 @@ public class Menu {
 			String x = printMenu();
 			switch(x){
 				case("1"):
-					login();
+					login.loginMenu();
 					break;
 				case("2"):
 					customer.addCustomerMenu();
@@ -65,140 +52,6 @@ public class Menu {
 		return true;
 	}
 	
-public void login() {
-		
-		String ID, password;
-		boolean userDone = false, passDone = false;
-		Connection con =null;
-		Statement statement =null;
-		ResultSet resultSet =null;
-		
-		do{
-			System.out.println("\nPlease Enter User ID or 'exit'"); 
-			ID = scan.next();
-			if(ID.matches("[c][0-9]{3}") | ID.matches("[o][0-9]{3}")) {
-				try {
-					con = DriverManager.getConnection("jdbc:sqlite:BookingSystem.db");
-					statement = con.createStatement();
-					
-					resultSet = statement.executeQuery("SELECT ownid FROM owner");
-					while(resultSet.next()) {
-						if(ID.equals(resultSet.getString("ownid"))) {
-							System.out.println("Admin found!");
-							userDone = true;
-							break;
-						}
-					}
-					resultSet.close();
-					if(!userDone) {
-						resultSet = statement.executeQuery("SELECT custid FROM customer");
-						while(resultSet.next()) {
-							if(ID.equals(resultSet.getString("custid"))) { 
-								System.out.println("User found!");
-								userDone = true;
-								break;
-							}
-						}
-						resultSet.close();
-					}
-				} catch (Exception e) {
-					System.err.println(e);
-				} finally {
-					//Closing statements for the sql connection
-					if (statement != null) {
-				        try {
-				            statement.close();
-				            System.out.println("id state closed");
-				        } catch (SQLException e) { System.out.println("id statment still open");}
-				    }
-				    if (con != null) {
-				        try {
-				            con.close();
-				            System.out.println("id con closed");
-				        } catch (SQLException e) { /* ignored */}
-				    }  
-				}
-				if(!userDone) {
-					System.out.println("No user found");
-					/* remove break to replay user ID input instead of going back to main menu */
-					break;
-				}
-					
-			} else if(ID.equals("exit")) {
-				System.out.println("You selected exit");
-				break;
-			} else {
-				System.out.println("Input Invalid");
-			}
-			
-		} while(!userDone);
-
-		if(userDone && !passDone) {
-			do {
-			System.out.println("\nPlease enter user password or 'exit'");
-			password = scan.next();
-				if(password.equals("exit")){
-					System.out.println("You selected exit");
-					break;
-				} else {
-					
-					try {
-						con = DriverManager.getConnection("jdbc:sqlite:BookingSystem.db");
-						statement = con.createStatement();
-						
-						//owner password check
-						resultSet = statement.executeQuery("SELECT password FROM owner WHERE ownID='" + ID + "'");
-						while(resultSet.next()) {
-							if(password.equals(resultSet.getString("password"))) {
-								System.out.println("Password correct!");
-								//moves to owner menu
-								statement.close();
-								con.close();
-								ownerMenu();
-								passDone = true;
-							}
-						}
-						
-						//customer password check
-						resultSet = statement.executeQuery("SELECT password FROM customer WHERE custid='" + ID + "'");
-						while(resultSet.next()) {
-							if(password.equals(resultSet.getString("password"))) { 
-								System.out.println("Password correct!");
-								//moves to customer menu
-								statement.close();
-								con.close();
-								customerMenu();
-								passDone = true;
-							} else {
-								System.out.println("Password incorrect");
-							}
-						}
-					} catch (Exception e) {
-						System.err.println(e);
-					} finally {
-						if(resultSet != null){
-					    	try {
-					    		resultSet.close();
-					    		System.out.println("pass result closed");
-					    	} catch (SQLException e){ /* ignored */ }
-					    }
-						if (statement != null) {
-					        try {
-					            statement.close();
-					            System.out.println("pass stat closed");
-					        } catch (SQLException e) { System.out.println("statement did not close");}
-					    }
-					    if (con != null) {
-					        try {
-					            con.close();
-					            System.out.println("pass con closed");
-					        } catch (SQLException e) { /* ignored */}
-					    } 
-					}
-				}
-			} while(!passDone);
-		}
-	}
 	
 	public void ownerMenu() throws IOException {
 		
@@ -213,7 +66,7 @@ public void login() {
 			System.out.println("\n2. Add working time/dates for next month");
 			System.out.println("\n3. Booking summary");
 			System.out.println("\n4. Employeee availability");
-			System.out.println("\n4. Logout");
+			System.out.println("\n5. Logout");
 			System.out.println("======================================");
 			System.out.println("Please enter you choice: ");
 			int choice = scan.nextInt();
@@ -222,7 +75,7 @@ public void login() {
 				case 1 : 
 					employee.addEmployeeMenu();
 					break;
-				case 4 :
+				case 5 :
 					System.out.println("\n");
 					BookingSystem.main(null);
 					break;
