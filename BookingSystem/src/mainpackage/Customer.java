9187ef253fda7c3010c61e08c9f299ad34a55795
rmit-s3599741-Khoa-Le <s3599741@student.rmit.edu.au>
2017-04-06 15:24:56 +1000

    Changed some codes for Customer and Employee

--- BookingSystem/src/mainpackage/Customer.java
+++ BookingSystem/src/mainpackage/Customer.java
@@ -1,7 +1,9 @@
 package mainpackage;
 
+import java.util.Scanner;
+
 public class Customer extends Person {
-	
+	final static Scanner scan = new Scanner(System.in);
 	public Customer(){
 		
 	}
@@ -12,93 +14,56 @@ public class Customer extends Person {
 	public void createBooking() {
 		// TODO Auto-generated method stub
 	}
+	
 	@Override
-	public void setID(String ID) {
-		while(true) {
-			if(ID.matches("[0-9]{3}")) {
-				ID.trim();
-				this.ID = ID;
-				break;
-			} else {
-				System.out.println("Invalid ID number");
-				break;
-			}
-		}		
+	public void setID(String ID) { //can't have duplicates
+		if(ID.matches("[0-9]{3}")) {
+			ID.trim();
+			this.ID = ID;
+		} else {
+			System.out.println("Invalid ID number");
+		}			
 	}
 	
 	@Override
 	public void setName(String name) {
-		while(true) {
-			if(name.matches("[a-zA-Z ]{1,30}")) {	
-				this.name = name;
-				break;
-			} else {
-				System.out.println("Invalid name");
-				break;
-			}
+		if(name.matches("[a-zA-Z ]{2,30}")) {	
+			this.name = name;		
+		} else {
+			System.out.println("Invalid name");			
 		}
 	}
 	
 	@Override
 	public void setPassword(String password) {	
-		while(true) {	
-			if(password.matches("[a-zA-Z0-9,./';:?><{}|+=-_()*&^%$#@!`~]{8,20}")){
-				this.password = password;
-				break;
-			} else {
-				System.out.println("Invalid Password");
-				break;
-			}
-		}		
+		if(password.matches("[a-zA-Z0-9,./';:?><{}|+=-_()*&^%$#@!`~]{8,20}")){
+			this.password = password;			
+		} else {
+			System.out.println("Invalid Password");		
+		}			
 	}
 	
 	@Override
 	public void setAddress(String address) {
-		while(true) {	
-			if(address.matches("[a-zA-Z0-9 ,./':-|_`~&$@#+()]{2,40}")){
-				this.address = address;
-				break;
-			} else {
-				System.out.println("Invalid Address");
-				break;
-			}
-		}		
+		if(address.matches("[a-zA-Z0-9 ,./':-|_`~&$@#+()]{2,40}")){
+			this.address = address;	
+		} else {
+			System.out.println("Invalid Address");		
+		}			
 	}
 	
 	@Override
 	public void setNumber(String number) {
-		while(true) {
-			if(number.matches("[0-9]{10}")){
-				this.number = number;
-				break;
-			} else {
-				System.out.println("Invalid Number");
-				break;
-			}
+		if(number.matches("[0-9]{10}")){
+			this.number = number;	
+		} else {
+			System.out.println("Invalid Number");		
 		}
 	}
-	/*@Override
-	public void setID(String ID) {
-		this.ID = ID;
-	}
-	@Override
-	public void setName(String name) {
-		this.name = name;
-	}
-	@Override
-	public void setAddress(String address) {
-		this.address = address;
-	}
-	@Override
-	public void setNumber(String number) {
-		this.number = number;
-	}
-	@Override
-	public void setPassword(String password) {
-		this.password = password;
-	}*/
+
 	@Override
 	public String toString(){
 		return ID + "," + name + "," + password + "," + address + "," + number;
 	}
+
 }
--- BookingSystem/src/mainpackage/Employee.java
+++ BookingSystem/src/mainpackage/Employee.java
@@ -1,42 +1,66 @@
 package mainpackage;
 
-import java.util.Scanner;
-
 public class Employee {
 	
 	
-	private String ID, name, address, number;
-	
-	public Employee() {
+	private String ID, name, address, position, age, number;
+	public Employee(){
 		
 	}
-	public Employee(String ID, String name, String address, String number) {
+	public Employee(String ID, String name, String address, String number, String age, String position) {
 		this.ID = ID;
 		this.name = name;
 		this.address = address;
 		this.number = number;
+		this.age = age;
+		this.position = position;
 	}
 
 	public String getID() { return ID; }
 	public String getName() { return name; }
 	public String getAddress() { return address; }
 	public String getNumber() { return number; }
+	public String getAge() { return age; }
+	public String getPosition() { return position; }
 	
 	public void setID(String ID) {
-		this.ID = ID;
+		if(ID.matches("[0-9]{3}")) {
+			ID.trim();
+			this.ID = ID;	
+		} else {
+			System.out.println("Invalid ID number");		
+		}	
 	}
 	public void setName(String name) {
-		this.name = name;
+		if(name.matches("[a-zA-Z ]{2,30}")) {	
+			this.name = name;
+		} else {
+			System.out.println("Invalid name");
+		}
 	}
-	public void setAddress(String address) {
-		this.address = address;
+	public void setAddress(String address) {	
+		if(address.matches("[a-zA-Z0-9 ,./':-|_`~&$@#+()]{2,40}")){
+			this.address = address;				
+		} else {
+			System.out.println("Invalid Address");			
+		}
+			
 	}
 	public void setNumber(String number) {
-		this.number = number;
+		if(number.matches("[0-9]{10}")){
+			this.number = number;
+		} else {
+			System.out.println("Invalid Number");
+		}
+	}
+	public void setAge(String age) {
+		this.age = age;
+	}
+	public void setPosition(String position) {
+		this.position = position;
 	}
 	
 	public String toString(){
-		return ID + "," + name +  "," + address + "," + number;
+		return ID + "," + name +  "," + address + "," + number + "," + age + "," + position;
 	}
-
 }
