69427df28d668afbce94156a8a742eab830e7cc2
Daniel <Daniel@Daniels-PC.gateway>
2017-04-09 18:57:16 +1000

    Add LoginValidation and Junits

---
+++ BookingSystem/src/test/LoginIDTest.java
@@ -0,0 +1,42 @@
+package test;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import mainpackage.LoginValidation;
+
+public class LoginIDTest {
+	
+	LoginValidation login = new LoginValidation();
+	@Test
+	public void test() {
+		assertTrue(1 == login.loginIDValidation("c001"));
+	}
+	
+	@Test 
+	public void test1() {
+		assertTrue(0 == login.loginIDValidation("c01"));
+	}
+	
+	@Test 
+	public void test2() {
+		assertTrue(0 == login.loginIDValidation("p001"));
+	}
+	
+	@Test 
+	public void test3() {
+		assertTrue(0 == login.loginIDValidation("abcd"));
+	}
+	
+	@Test 
+	public void test4() {
+		assertTrue(0 == login.loginIDValidation("c1"));
+	}
+	
+	@Test 
+	public void test5() {
+		assertTrue(1 == login.loginIDValidation("o001"));
+	}
+
+}
